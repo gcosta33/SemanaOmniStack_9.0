6dfa54865d39786e2ab67d1ad922ea1e5a712ba0
guilherme3394 <guilherme3394@gmail.com>
2019-10-03 00:41:38 -0300

    adição do banco mongoDB e efetuada a conexão

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes')
 
 const app = express();
@@ -11,6 +12,10 @@ const app = express();
 // req.query = Acessar query params(para filtros)
 // req.params = Acessar route params(para edição,delete)
 
+mongoose.connect('mongodb+srv://gcostaOmni:gcostaOmni@omnistack-jeayd.mongodb.net/semana09?retryWrites=true&w=majority',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
