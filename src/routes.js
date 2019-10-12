d31442000be765abdd8d8595a9689d74d00b2d11
guilherme3394 <guilherme3394@gmail.com>
2019-10-12 13:07:28 -0300

    criação do controllers booking e dashboard, modelo de booking. Rotas para criação de reserva e listagem de spots

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
-const SessionController = require('./controllers/SessionController')
-const SpotController = require('./controllers/SpotController')
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -13,6 +15,10 @@ routes.get('/', (req, res) => {
 });
 
 routes.post('/sessions', SessionController.store);
-routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+routes.get('/dashboard',DashboardController.show);
+routes.post('/spots/:spot_id/bookings', BookingController.store);
+
 
 module.exports = routes;
